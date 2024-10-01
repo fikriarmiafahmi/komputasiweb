6081a9bdd9000c74c3165bd9e39ce09cf96269dc
fikriarmiafahmi <46241787+fikriarmiafahmi@users.noreply.github.com>
2024-10-01 11:09:24 +0700

    Add files via upload

---
+++ pertemuan4/perintah.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE kontak;
+USE kontak;
+CREATE TABLE kontak(IDUser INT, nama varchar(50), email varchar(50), gender varchar(50), tanggal_lahir varchar(50), hobby varchar(50));
+SELECT * FROM kontak;
+
+
+
